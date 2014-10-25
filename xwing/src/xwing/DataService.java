3b253157bee80f4b0c4fcf96c0d039d4dfab0cc4
i4c8 <i4c8@ugrad.cs.ubc.ca>
2014-10-24 23:20:02 -0700

    Added java-callgraph to buildpath. Can call java-callgraph from within our program. [Buggy/untested: halted with a null pointer after running for some reason.]

--- xwing/src/xwing/DataService.java
+++ xwing/src/xwing/DataService.java
@@ -1,11 +1,27 @@
 package xwing;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import gr.gousiosg.javacg.stat.ClassVisitor;
+import gr.gousiosg.javacg.stat.JCallGraph;
+import gr.gousiosg.javacg.stat.MethodVisitor;
+
 public class DataService {
+	
 	// Main method
 	public static void main(String[] args){
 		
 		System.out.println("Hello world.");
 		
+		String[] argsOut = new String[5];
+		argsOut[0] = "javacg-0.1-SNAPSHOT-static.jar";
+		
+		JCallGraph.main(argsOut);
+		
 	}
+	
+
 
 }
