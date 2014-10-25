f96a3d4d2c3bb5615f56353958db1d45868265b7
i4c8 <i4c8@ugrad.cs.ubc.ca>
2014-10-25 00:27:32 -0700

    Writes results of java-callgraph to a file. (Currently results.txt but can change this with args or in code)

--- xwing/src/xwing/DataService.java
+++ xwing/src/xwing/DataService.java
@@ -1,24 +1,79 @@
 package xwing;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 import gr.gousiosg.javacg.stat.ClassVisitor;
 import gr.gousiosg.javacg.stat.JCallGraph;
 import gr.gousiosg.javacg.stat.MethodVisitor;
 
 public class DataService {
 	
-	// Main method
+	// Main Method
+	/**
+	 * @param args - Specified .jar file at runtime in console. 
+	 */
 	public static void main(String[] args){
+		System.out.println("Welcome to XWing!\r\n");
+		//	If we decide to specify within the program rather than
+		//	in the runtime arguments, build from here.
+		//System.out.println("Specify .jar name: ");
 		
-		System.out.println("Starting java-callgraph on JAR: " + args[0]);
+		// Call java-callgraph
+		File result = null;
+		try {
+			
+			result = runCallGraph(args, "result.txt");
+			//System.out.print("\r\n\r\n" + "printing Results: ");
+			//System.out.println(result);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		//String[] argsOut = new String[1];
-		//argsOut[0] = "/lib/javacg-0.1-SNAPSHOT-static.jar";
+	}
+	
+	/**
+	 * @param args
+	 * @return File with name fname containing the results of the java-callgraph call
+	 * @throws IOException
+	 */
+	private static File runCallGraph(String[] args, String fname) throws IOException{
+		System.out.println("Creating file: " + fname);
+		File result = new File(fname);
 		
-		JCallGraph.main(args);
+		System.out.println("Running java-callgraph.");
+		// java-callgraph prints each method to the console. So I'm just going to capture it.
+		// We can also modify the source and rebuild (or get rid of) the parser, but we've already got the parser
+		// working on files. Consider refactoring if time.
+		OutputStream out = new FileOutputStream(result);
+	    //ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	    PrintStream ps = new PrintStream(out);
+	    // Save the old System.out!
+	    PrintStream old = System.out;
+	    // Tell Java to use your special stream
+	    System.setOut(ps);
+	    
+	    // Call JCallGraph on jar name in args
+	    JCallGraph.main(args);
+	    // Put things back
+	    System.out.flush();
+	    System.setOut(old);
+	    System.out.println("Finished java-callgraph!");
+
+	    if(result.exists()){
+	    	System.out.println("Results are in: " + fname);
+	    } else
+	    	System.out.println("Error creating results file.");
+	    
+		return result;
 		
 	}
 	
