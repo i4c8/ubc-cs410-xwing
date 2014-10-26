5a0c6bc4c252cd2310ab13b5f22a8e41a5d44115
i4c8 <i4c8@ugrad.cs.ubc.ca>
2014-10-26 15:24:48 -0700

    Refactored git operations to separate class. Added a call to the parser in DataService to demonstrate architecture.

---
+++ xwing/src/crawl/Crawler.java
@@ -0,0 +1,34 @@
+package crawl;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+public class Crawler {
+	
+	public Crawler(){
+		//
+	}
+	
+	//TODO
+	public static Git getGit(Repository repo){
+		//Repository repo;
+		Git git = new Git(repo);
+		
+		return git;
+	}
+	
+	public static void walkRepo(Repository repo){
+		// TODO Figure out how to access the outside repo we want to analyze
+		RevWalk walk = new RevWalk(repo);
+		
+		for(RevCommit commit : walk){
+			// TODO We need to compile the commit to a jar somehow and run it through callgraph
+			System.out.println("Commit name: " + commit.getName());
+		}
+		
+		// Cleanup
+		walk.dispose();
+	}
+}
