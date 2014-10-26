20c90c160ba42e9781c18fd77a2d8647161f877f
asommerey <alex_sommerey@hotmail.com>
2014-10-25 19:26:32 -0700

    Changed some of the ParserTests to be automatic

--- xwing/src/xwing/CallGraphParserTest.java
+++ xwing/src/xwing/CallGraphParserTest.java
@@ -1,21 +1,38 @@
 package xwing;
 
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+
 import org.junit.Test;
 
 /*
- * The last modifications I did of CallgraphParser these all passed as of Oct 25/ 7:00 pm
+ * All Tests automatic save for the last which has instructions to manually performed
  */
 public class CallGraphParserTest {
 	
 	/*
-	 * After running this test check temp10 for the String java
-	 * Note: Java is okay, we are only trying to remove instances 
-	 * of provided classes like java.util
+	 * Tests that each line does not contain java
 	 */
 	@Test
 	public void removeJavaTest(){
 		CallgraphParser test = new CallgraphParser();
 		test.removeJava("result.txt", 10);
+		String line;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("temp10"));
+			while ((line = br.readLine()) != null){
+				assertEquals("Line does not contain \"java\"", false, line.contains("java"));
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 	
 	/*
@@ -24,7 +41,20 @@ public class CallGraphParserTest {
 	@Test
 	public void removeDuplicatesTest(){
 		CallgraphParser test = new CallgraphParser();
+		HashSet<String> data = new HashSet<String>();
 		test.removeDuplicates("result.txt", 11);
+		String line;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("temp11"));
+			while ((line = br.readLine()) != null){
+				assertEquals("Line does not contain duplicates", false, data.contains(line));
+				data.add(line);
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	/*
@@ -35,8 +65,38 @@ public class CallGraphParserTest {
 	public void splitMethodsAndClassesTest(){
 		CallgraphParser test = new CallgraphParser();
 		test.splitClassesMethods("result.txt", 12);
+		this.testMethods();
+		this.testClasses();
 	}
 	
+	@Test
+	public void testClasses() {
+		String line;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("temp12.classes"));
+			while ((line = br.readLine()) != null){
+				assertEquals("Line begins with C", 'C', line.charAt(0));
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testMethods() {
+		String line;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("temp12.methods"));
+			while ((line = br.readLine()) != null){
+				assertEquals("Line begins with M", 'M', line.charAt(0));
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/*
 	 * Compare result0.json with result1.json
 	 * They should both be in the appropriate format as discussed on piazza
