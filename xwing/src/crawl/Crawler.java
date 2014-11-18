7b04659b594fbc47553175a4121f68938cafd125
Norris Ng <norrisng@users.noreply.github.com>
2014-11-18 15:22:31 -0800

    Further work on crawling, esp. on returned list and creating JAR

--- xwing/src/crawl/Crawler.java
+++ xwing/src/crawl/Crawler.java
@@ -1,11 +1,18 @@
 package crawl;
 
+import jar.JarHelper;
+
 import java.awt.List;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.DepthWalk.Commit;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 
 public class Crawler {
@@ -22,49 +29,56 @@ public class Crawler {
 		return git;
 	}
 	
-	public static void walkRepo(Repository repo){
+	public ArrayList<String> walkRepo(Repository repo){
 		// TODO Figure out how to access the outside repo we want to analyze
 		RevWalk walk = new RevWalk(repo);
-		List authJar = new List();
+		ArrayList<String> authJar = new ArrayList<String>();
 		
 		for(RevCommit commit : walk){
 			// TODO We need to compile the commit to a jar somehow and run it through callgraph
 			
 			String commitName = commit.getName();
-			newJar(commitName);
+			File jarFile = newJar(commitName);
 			
 			// TODO: (1) shove all directories and java files into JAR
 			// 				Refer to: 	https://stackoverflow.com/questions/2977663/java-code-to-create-a-jar-file
 			//							http://www.java2s.com/Code/Java/File-Input-Output/CreateJarfile.htm (using java.util.jar)
 			//					(refer to these if JarHelper doesn't work)
-			
-			
+			RevTree fileTree = commit.getTree();
+			JarHelper jh = new JarHelper();
+			fileTree.getType();
+//			jh.jarDir(dirOrFile2Jar, jarFile);
 			
 			// TODO: (2) get author of commit and JAR filename, and add to a list
 
 			String authorName = commit.getAuthorIdent().getName();
-			authJar.add("[ " + authorName + ", " +" ]");
 			
-			System.out.println("Commit name: " + commitName);
+			// Note: returning jarFile *might* be better idea?
+			authJar.add("{ " + authorName + ", " + commitName +" }");
+			
 		}
 		
 		// Cleanup
 		walk.dispose();
 		
-		// TODO: (3) return above said list
+		// (3) return above said list
+		return authJar;
 	}
 	
 	/**
-	 * Creates an empty JAR file
+	 * Creates an empty JAR file.
 	 * 
-	 * @param filename	filename of JAR to be created
+	 * @param filename	filename of JAR to be created, without the extension
+	 * @return			the newly-created JAR (returns null if IOException encountered)
 	 */
-	private static void newJar(String filename) {
+	private File newJar(String filename) {
 		try {
 			File commitJar = new File(filename + ".jar");
-			commitJar.createNewFile();
-		} catch (Exception e) {
+			commitJar.createNewFile();	
+			return commitJar;			
+		} catch (IOException e) {
 			e.printStackTrace();
+			return null;
 		}
 	}
 	
